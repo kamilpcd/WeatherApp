224144e15ce6ff725b24cef8eda376d73a672246
kamilpcd <kamil.poszwa@gmail.com>
2024-03-09 16:01:08 +0100

    Pressure and wind speed implementation

--- script.js
+++ script.js
@@ -6,18 +6,25 @@ const photo = document.querySelector('.photo')
 const weather = document.querySelector('.weather')
 const temperature = document.querySelector('.temperature')
 const humidity = document.querySelector('.humidity')
+const pressure = document.querySelector('.pressure')
+const windSpeed = document.querySelector('.windSpeed')
+const unitSelect = document.querySelector('.unit-select');
+
+
 
 import API_KEY from './config/config.js';
 
 const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q='
 const USER_API_KEY = '&appid=' + API_KEY
-const API_UNITS = '&units=metric'
+const API_UNITS = '&units='
 
 
 const getWeather = () => {
-    const city = input.value || 'London';
-    const URL = API_LINK + city + USER_API_KEY + API_UNITS;
+    const city = input.value;
+    const unitsToRequest = unitSelect.value;
+    const URL = API_LINK + city + USER_API_KEY + API_UNITS + unitsToRequest;
 
+    console.log(unitsToRequest)
     axios
         .get( URL )
         .then( ( res ) => updateWeatherDisplay( res.data ) )
@@ -26,14 +33,31 @@ const getWeather = () => {
 
 const updateWeatherDisplay = ( data )=> {
     const temp = data.main.temp;
+    const wind = data.wind.speed;
     const hum = data.main.humidity;
+    const press = data.main.pressure;
     const status = Object.assign( {}, ...data.weather );
 
+    if ( unitSelect.value === 'metric' )
+    {
+        temperature.textContent = Math.floor( temp ) + '℃';
+    }
+    else
+    {
+        temperature.textContent = Math.floor( temp ) + '℉';
+    }
     cityName.textContent = data.name;
-    temperature.textContent = Math.floor( temp ) + '℃';
     humidity.textContent = hum + '%';
     weather.textContent = status.main;
-
+    pressure.textContent = press + " hPa";
+    if ( unitSelect.value === 'metric' )
+    {
+        windSpeed.textContent = Math.round( wind ) + " km/h";
+    }
+    else
+    {
+        windSpeed.textContent = Math.round( wind ) + " mph";
+    }
     warning.textContent = '';
     input.value = '';
 
@@ -80,14 +104,12 @@ const handleApiError = () => {
     warning.textContent = 'City name is incorrect!';
 };
 
-
-
 const enterCheck = (e) => {
-    if (e.key === 'Enter') {
+    if ( e.key === 'Enter' ) {
         getWeather();
     }
 };
 
-getWeather();
+
 input.addEventListener('keyup', enterCheck);
 button.addEventListener('click', getWeather);
